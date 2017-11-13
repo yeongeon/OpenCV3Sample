511f9b77948bb4366e9c644438653de7420cf2fb
Lewis.kim <lewis.kim@kakaobank.com>
2017-11-13 21:43:55 +0900

    add view for camera.

---
+++ OpenCV3Sample/CameraUtil.swift
@@ -0,0 +1,33 @@
+//
+//  CameraUtil.swift
+//  OpenCV3Sample
+//
+//  Created by lewis.kim on 13/11/2017.
+//  Copyright © 2017  yeongeon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class CameraUtil {
+    class func imageFromSampleBuffer(buffer: CMSampleBuffer) -> UIImage {
+        let pixelBuffer: CVImageBuffer = CMSampleBufferGetImageBuffer(buffer)!
+        
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        
+        let pixelBufferWidth = CGFloat(CVPixelBufferGetWidth(pixelBuffer))
+        let pixelBufferHeight = CGFloat(CVPixelBufferGetHeight(pixelBuffer))
+        let imageRect: CGRect = CGRectMake(0, 0, pixelBufferWidth, pixelBufferHeight)
+        let ciContext = CIContext.init()
+        let cgimage = ciContext.createCGImage(ciImage, from: imageRect )
+        
+        let image = UIImage(cgImage: cgimage!)
+        return image
+    }
+    
+    class func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+}
+
--- OpenCV3Sample/ViewController.swift
+++ OpenCV3Sample/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import AVKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var canvas: UIView!
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var session : AVCaptureSession!
+    var device : AVCaptureDevice!
+    var output : AVCaptureVideoDataOutput!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+        if initCamera() {
+            session.startRunning()
+        }
+        
         let t = OpenCVWrapper()
         let v:String = t.openCVVersionString()
         log.debug("+++++ v: \(v)")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +36,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func initCamera() -> Bool {
+        session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.medium
+        
+        let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
+                                             for: AVMediaType.video,
+                                             position: AVCaptureDevice.Position.back)
+        if device == nil {
+            return false
+        }
+        
+        do {
+            let myInput: AVCaptureDeviceInput?
+            try myInput = AVCaptureDeviceInput(device: device!)
+            
+            if session.canAddInput(myInput!) {
+                session.addInput(myInput!)
+            } else {
+                return false
+            }
+            
+            output = AVCaptureVideoDataOutput()
+            output.videoSettings = [ kCVPixelBufferPixelFormatTypeKey as AnyHashable as! String: Int(kCVPixelFormatType_32BGRA) ]
+            
+            try device?.lockForConfiguration()
+            device?.activeVideoMinFrameDuration = CMTimeMake(1, 15)
+            device?.unlockForConfiguration()
+            
+            let queue: DispatchQueue = DispatchQueue(label: "myqueue", attributes: [])
+            output.setSampleBufferDelegate(self, queue: queue)
+            
+            output.alwaysDiscardsLateVideoFrames = true
+        } catch let error as NSError {
+            print(error)
+            return false
+        }
+        
+        if session.canAddOutput(output) {
+            session.addOutput(output)
+        } else {
+            return false
+        }
+        
+        for connection in output.connections {
+            if connection.isVideoOrientationSupported {
+                connection.videoOrientation = AVCaptureVideoOrientation.portrait
+            }
+        }
+        
+        return true
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
+        DispatchQueue.main.async(execute: {
+            let image: UIImage = CameraUtil.imageFromSampleBuffer(buffer: sampleBuffer)
+            self.imageView.image = image;
+        })
+    }
+    
 
 }
 
